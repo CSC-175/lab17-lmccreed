931b1bab6356f0a16ef0aa2447e677d5d22e1024
administrator <lmccreed@umich.edu>
2024-11-20 09:41:59 -0500

    1

--- lab17.cpp
+++ lab17.cpp
@@ -1,1 +1,11 @@
 // Definition of findHigh function goes here
+template <typename A, typename B, typename C, typename D>
+
+ void findHigh(A data[], B size, C &position, D &maxvalue) {
+    for (int i = 0; i < size; i++) {
+        if (data[i] > maxvalue) {
+            maxvalue = data[i];
+            position = i;
+        }
+    }
+    }
